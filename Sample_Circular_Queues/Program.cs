68bea724e0e23fce5ac450df029cae4346abcd9f
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-12-16 17:33:05 +0700

    add public void remove

--- Sample_Circular_Queues/Program.cs
+++ Sample_Circular_Queues/Program.cs
@@ -19,6 +19,23 @@ namespace CircularQueues_CSharp
                 Console.WriteLine("\nQueue overflow\n");
                 return;
             }
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    REAR = REAR + 1;
+            }
+            queue_array[REAR] = element;
+        }
+        public void remove()
+        {
+
         }
     }
 }
