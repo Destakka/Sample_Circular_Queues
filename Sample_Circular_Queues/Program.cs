063f851c3bd00da15cf980d75b8b3c39fe4b8ac0
DESTA ATIKANANDA <d.atikananda.ft21@mail.umy.ac.id>
2022-12-16 17:30:53 +0700

    add public void insert

--- Sample_Circular_Queues/Program.cs
+++ Sample_Circular_Queues/Program.cs
@@ -4,13 +4,21 @@ namespace CircularQueues_CSharp
 {
     class Queues
     {
-        int FRONT, NEAR, max = 5;
+        int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
 
         public Queues()
         {
             FRONT = -1;
             REAR = -1;
+        }
+        public void insert(int element)
+        {
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR +1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
         }
     }
 }
